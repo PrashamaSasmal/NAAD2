278b90c441cd545e5c254937c04fa4c974bb41f8
PrashamaSasmal <prashamasasmal65@gmail.com>
2021-03-26 19:14:11 +0530

    WAP to Find ASCII Value of a Character

---
+++ Task0/Introduction/Task0_4.java
@@ -0,0 +1,10 @@
+//WAP to Find ASCII Value of a Character
+public class Task0_4 {
+    public static void main(String args[]){
+        char ch='c';
+        int castAscii=ch;
+        System.out.println("ASCII value of "+ch+" is: "+castAscii);
+    
+    }
+    
+}
