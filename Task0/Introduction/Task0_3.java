656239f4408b592cb64f1feae4b825dee2c90c58
PrashamaSasmal <prashamasasmal65@gmail.com>
2021-03-26 19:13:28 +0530

    WAP to Multiply Two Floating Point Numbers

---
+++ Task0/Introduction/Task0_3.java
@@ -0,0 +1,10 @@
+
+//WAP to Multiply two Floating point Numbers
+public class Task0_3 {
+    public static void main(String args[]){
+        float num1=23.5f;
+        float num2=45.2f;
+        float multiply=num1*num2;
+        System.out.println("product : "+num1+" * "+num2+" = "+multiply);
+    }
+}
