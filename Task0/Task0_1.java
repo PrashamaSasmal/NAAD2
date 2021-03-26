d93aea2ece2606894b7704b38f188f1c24bb3647
PrashamaSasmal <prashamasasmal65@gmail.com>
2021-03-26 12:27:28 +0530

    WAP to print an integer number

---
+++ Task0/Task0_1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Task0_1
+ {
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter a integer number=");
+        int number=input.nextInt();
+        System.out.println("You entered ="+number);
+
+    }
+}
