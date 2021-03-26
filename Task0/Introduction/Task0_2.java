19e94f4ef47eee9e8a04a39931704c9eda632160
PrashamaSasmal <prashamasasmal65@gmail.com>
2021-03-26 19:11:09 +0530

    WAP to Add Two Integers

---
+++ Task0/Introduction/Task0_2.java
@@ -0,0 +1,12 @@
+//WAP to Add two Integers
+public class Task0_2 {
+    public static void main(String args[]){
+        
+        int num1=12;
+        int num2=20;
+        int sum=num1+num2;
+        System.out.println("Sum : "+num1+" + "+num2+" = "+sum);
+    }
+    
+    
+}
