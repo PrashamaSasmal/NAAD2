b2143035fd08f21980615c05130990d46f60e562
PrashamaSasmal <prashamasasmal65@gmail.com>
2021-03-26 19:09:44 +0530

    WAP to Print Integer

---
+++ Task0/Introduction/Task0_1.java
@@ -0,0 +1,13 @@
+//WAP to Print Integer
+import java.util.Scanner;
+public class Task0_1
+ {
+    public static void main(String[] args)
+    {
+        System.out.println("Enter a integer number=");
+        Scanner input=new Scanner(System.in);
+        int number=input.nextInt();
+        System.out.println("You entered ="+number);
+
+    }
+}
