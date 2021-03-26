44463490da0b42ee12d0b1c07671f71ccf385a8b
PrashamaSasmal <prashamasasmal65@gmail.com>
2021-03-26 19:16:17 +0530

    WAP to Swap Two Numbers

---
+++ Task0/Introduction/Task0_6.java
@@ -0,0 +1,13 @@
+//WAP to Swap Two Numbers
+public class Task0_6 {
+    public static void main(String args[]){
+    int num1=20,num2=30;
+    System.out.println("Numbers before Swapping: "+num1+" "+num2);
+    int temp=num1;
+    num1=num2;
+    num2=temp;
+    System.out.println("Numbers after Swapping: "+num1+" "+num2);
+   
+}
+    
+}
