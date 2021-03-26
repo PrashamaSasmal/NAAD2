f73f7c21cef2eedb59ce5d8e4d0a5e27c279d969
PrashamaSasmal <prashamasasmal65@gmail.com>
2021-03-26 19:15:07 +0530

    WAP to Compute Quotient and Remainder

---
+++ Task0/Introduction/Task0_5.java
@@ -0,0 +1,14 @@
+//WAP to Compute Quotient and Remainder
+public class Task0_5 {
+    public static void main(String args[]){
+
+    int dividend=45;
+    int divisor=7;
+    int quotient=dividend/divisor;
+    int remainder=dividend%divisor;
+    System.out.println("Quotient :"+quotient);
+    System.out.println("Remainder :"+remainder);
+    
+}
+    
+}
