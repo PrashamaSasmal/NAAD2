b7cfd06164e7da18a644e1ac2786645ea8b51c1c
PrashamaSasmal <prashamasasmal65@gmail.com>
2021-03-27 22:57:28 +0530

    //WAP to Display Alphabets(A to Z)using loop

---
+++ Task0/Decision_making_and_Loops/Task0_19.java
@@ -0,0 +1,9 @@
+//WAP to Display Alphabets(A to Z)using loop
+public class Task0_19 {
+    public static void main(String[] args){
+    
+    for(char i=65;i<=90;i++)
+    System.out.println(i+" ");
+    }
+    //for(char i=65;i<=90;i++) can also use this
+}
