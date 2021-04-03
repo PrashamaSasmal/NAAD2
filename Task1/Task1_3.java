fcf751296b68a2b84d4ea3dda204b71695d6e4a0
PrashamaSasmal <prashamasasmal65@gmail.com>
2021-04-03 22:50:37 +0530

    Task 1.3 isDirectlyProportional()

---
+++ Task1/Task1_3.java
@@ -0,0 +1,44 @@
+//Task 1.3 isDirectlyProportional()
+
+import java.util.Scanner;
+public class Task1_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array:");
+        int n = sc.nextInt();
+      
+        int[] a = new int[n];
+        int[] b = new int[n];
+        
+        System.out.println("Enter values for x:");
+        for(int i=0;i<n;i++)
+        a[i] = sc.nextInt();
+        System.out.println("Enter values for y:");
+        for(int i=0;i<n;i++)
+        b[i] = sc.nextInt();
+        
+        boolean answer = isDirectlyProportional( a , b , n);
+        
+        if(answer)
+        System.out.println("X and Y are directly proportional to each other.");
+        else
+        System.out.println("X and Y are not directly proportional to each other.");
+        
+    }
+     public  static boolean isDirectlyProportional(int[] x , int[] y ,int n){
+      
+      float value = x[0]/y[0];
+      boolean result = true;
+      float [] array = new float[n];
+      for(int i = 1; i <n; i++){
+      array[i] = (float) x[i]/y[i];
+       
+        if(value == array[i])
+         result = true;
+         else 
+         result = false;
+       }
+       
+   return result;
+   }    
+}
